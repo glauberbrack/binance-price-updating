49b2f0145e43ddb1852a0430e7cd85bcb67e5850
glauberbrack <glauber@brack.com.br>
2024-05-23 15:04:50 -0300

    refactor(types): update symbols

--- src/@types/symbol.d.ts
+++ src/@types/symbol.d.ts
@@ -17,8 +17,49 @@ declare type TSymbols = {
   cancelReplaceAllowed: boolean;
   isSpotTradingAllowed: boolean;
   isMarginTradingAllowed: boolean;
+  filters: Filter[];
   permissions: string[];
   permissionSets: string[][];
   defaultSelfTradePreventionMode: string;
   allowedSelfTradePreventionModes: string[];
+  lastPrice?: string;
+  bidPrice?: string;
+  askPrice?: string;
+  priceChange?: string;
+};
+
+type Filter = {
+  filterType: string;
+  [key: string]: any;
+};
+
+declare type TSymbolInfo = {
+  A: string;
+  B: string;
+  C: number;
+  E: number;
+  F: number;
+  L: number;
+  O: number;
+  P: string;
+  Q: string;
+  a: string;
+  b: string;
+  c: string;
+  e: string;
+  h: string;
+  l: string;
+  n: number;
+  o: string;
+  p: string;
+  q: string;
+  s: string;
+  v: string;
+  w: string;
+  x: string;
+};
+
+declare type TSymbolsWebSocket = {
+  stream: string;
+  data: TSymbolInfo;
 };
