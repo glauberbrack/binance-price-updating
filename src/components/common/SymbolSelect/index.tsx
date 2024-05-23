8bad10f82b94684bc1d40efe645b3fe1e04179d3
glauberbrack <glauber@brack.com.br>
2024-05-23 15:17:53 -0300

    feat(select): enhance perfomance with memo

--- src/components/common/SymbolSelect/index.tsx
+++ src/components/common/SymbolSelect/index.tsx
@@ -1,32 +1,28 @@
-import React from "react";
+import React, { memo } from "react";
 
 import { useSymbols } from "../../../context/SymbolsContext";
 import { ListItem } from "./styles";
 
-export const SymbolSelect = ({
-  index,
-  style,
-}: {
-  index: number;
-  style: React.CSSProperties;
-}) => {
-  const { symbols, selectedSymbols, addSymbol, deleteSymbol } = useSymbols();
-  const symbol = symbols[index];
+export const SymbolSelect = memo(
+  ({ index, style }: { index: number; style: React.CSSProperties }) => {
+    const { symbols, selectedSymbols, addSymbol, deleteSymbol } = useSymbols();
+    const symbol = symbols[index];
 
-  const handleToggleSymbol = (symbol: (typeof symbols)[0]) => {
-    selectedSymbols.some((s) => s.symbol === symbol.symbol)
-      ? deleteSymbol(symbol)
-      : addSymbol(symbol);
-  };
+    const handleToggleSymbol = (symbol: (typeof symbols)[0]) => {
+      selectedSymbols.some((s) => s.symbol === symbol.symbol)
+        ? deleteSymbol(symbol)
+        : addSymbol(symbol);
+    };
 
-  return (
-    <ListItem style={style}>
-      <input
-        type="checkbox"
-        onChange={() => handleToggleSymbol(symbol)}
-        checked={selectedSymbols.some((s) => s.symbol === symbol.symbol)}
-      />
-      {symbol.symbol}
-    </ListItem>
-  );
-};
+    return (
+      <ListItem style={style}>
+        <input
+          type="checkbox"
+          onChange={() => handleToggleSymbol(symbol)}
+          checked={selectedSymbols.some((s) => s.symbol === symbol.symbol)}
+        />
+        {symbol.symbol}
+      </ListItem>
+    );
+  }
+);
